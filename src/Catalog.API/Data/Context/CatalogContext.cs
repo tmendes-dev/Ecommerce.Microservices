4dd78835261abecd690060aaaa2f8ec77159db28
Thomas Mendes <th.mendes1997@gmail.com>
2023-08-06 01:47:20 -0300

    feat: catalog api init, mongodb config, dbcontext and product repository

---
+++ src/Catalog.API/Data/Context/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Interfaces.Data.Context;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data.Context;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration configuration)
+    {
+        MongoClient client = new MongoClient(configuration.GetValue<string>("DatabaseSetting:ConnectionString"));
+        IMongoDatabase? database = client.GetDatabase(configuration.GetValue<string>("DatabaseSetting:Database"));
+        Products =
+            database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSetting:CollectionName"));
+        CatalogContextSeed.SeedData(Products);
+    }
+
+    public IMongoCollection<Product> Products { get; }
+}
---
+++ src/Catalog.API/Interfaces/Data/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Interfaces.Data.Repository;
+
+public interface IProductRepository
+{
+    Task Create(Product product);
+    Task<bool> Delete(string id);
+    Task<bool> Update(Product product);
+    Task<Product> GetById(string id);
+    Task<IEnumerable<Product>> GetAll();
+    Task<IEnumerable<Product>> GetByName(string name);
+    Task<IEnumerable<Product>> GetByCategory(string categoryName);
+}
